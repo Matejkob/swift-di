e964ecca419aa0ad7eeb4f946eb2e1465f368281
Mateusz Bąk <bakmatthew@icloud.com>
2025-05-31 19:05:21 +0200

    Init empty SwiftPM

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-di",
+    products: [
+        .library(
+            name: "DI",
+            targets: ["DI"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "DI"
+        ),
+        .testTarget(
+            name: "DITests",
+            dependencies: ["DI"]
+        ),
+    ]
+)
