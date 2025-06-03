c656e5b00e439035b9c91cc0c99d90ee001b829a
Mateusz Bąk <bakmatthew@icloud.com>
2025-06-03 22:55:24 +0200

    Add Dependency property wrapper for dependency injection

---
+++ Sources/DI/Dependency.swift
@@ -0,0 +1,12 @@
+@propertyWrapper public struct Dependency<Value>: Sendable {
+  
+  public let keyPath: KeyPath<DependencyContainer, Value> & Sendable
+  
+  public var wrappedValue: Value {
+    DependencyContainer.current[keyPath: keyPath]
+  }
+  
+  @inlinable public init(_ keyPath: KeyPath<DependencyContainer, Value> & Sendable) {
+    self.keyPath = keyPath
+  }
+}
